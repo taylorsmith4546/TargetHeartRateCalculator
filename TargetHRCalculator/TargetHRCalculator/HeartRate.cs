99db0428cf4aeba9e96a96459913a2c3410b8a68
Taylor Smith <taylorsmith4546@gmail.com>
2016-04-27 09:33:44 -0700

    first commit

---
+++ TargetHRCalculator/TargetHRCalculator/HeartRate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetHRCalculator
+{
+    class HeartRate
+    {
+
+        //private members
+
+        private string _firstName;
+        private string _lastName;
+        private int _yearOfBirth;
+
+        public HeartRate(string firstName, string lastName, int yearOfBirth)
+        {
+            //getting the current year
+            CurrentYear = DateTime.Today.Year;
+            //assign values passed into constructor
+            FirstName = firstName;
+            LastName = lastName;
+            YearOfBirth = yearOfBirth;
+        }
+
+        //assigning first name, if provided
+        public string FirstName
+        {
+            get { return _firstName;}
+            set { _firstName = value == "" ? "n/a" : value;}
+        }
+
+        //assign last name, if provided
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value == "" ? "n/a" : value;}
+        }
+        //assign year of birth
+        //if not provided, default to 0
+        public int YearOfBirth
+        {
+            get { return _yearOfBirth; }
+            set { _yearOfBirth = value < 1 ? 0 : value;}
+        }
+        //current year is set in constructor only
+        //cannot be set by user
+        public readonly int CurrentYear;
+
+        //calculate person's age
+        public int PersonAge
+        {
+            get { return CurrentYear - YearOfBirth;}
+        }
+        //calculate maximum heart rate
+        public int MaxHeartRate
+        {
+            get { return 220 - PersonAge;}
+        }
+        //calculate minumum heart rate (50% of max)
+        public int MinTargetHeartRate
+        {
+            get { return Convert.ToInt32(MaxHeartRate * 0.5); }
+        }
+        //calculate maxiumum target heart rate (85% of max)
+        public int MaxTargetHeartRate
+        {
+            get { return Convert.ToInt32(MaxHeartRate * 0.85); }
+        }
+    }
+    }
+
---
+++ TargetHRCalculator/TargetHRCalculator/program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetHRCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name: ");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name: ");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your year of birth: ");
+            var yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            var heartRate = new HeartRate(firstName, lastName, yearOfBirth);
+
+            Console.WriteLine("Your name is {0} {1}", heartRate.FirstName, heartRate.LastName);
+            if (heartRate.YearOfBirth != 0)
+            {
+                Console.WriteLine("Your age is {0}", heartRate.PersonAge);
+                Console.WriteLine("Your max safe heart rate is {0}", heartRate.MaxHeartRate);
+                Console.WriteLine("Your max target heart rate is {0}", heartRate.MaxTargetHeartRate);
+                Console.WriteLine("Your min target heart rate is {0}", heartRate.MinTargetHeartRate);
+            }
+            else
+            {
+                Console.WriteLine("Calculations could not be performed because you did not provide a valid birth year");
+            }
+            
+
+            Console.ReadKey();         
+        }
+    }
+}
